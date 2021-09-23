f5e26042101082dfdac4307a966831e276a24fb8
Roger Medrano <email@example.com>
2021-09-23 09:49:40 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloYall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! C# HERE I COME!!!");
+        }
+    }
+}
